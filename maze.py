538be09a6b320c2c793bf8991efe6b5cedbda8b7
Vignesh Sekar <vigneshsekar1996@gmail.com>
2024-11-17 17:46:45 +0530

    WIP fixed issues in maze generation

--- maze.py
+++ maze.py
@@ -24,6 +24,8 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         if rows !=0 and cols!=0:
+            # self._cells[0][0].has_bottom_wall = False
+            # self._cells[0][0].draw()
             self._break_walls_r(0,0)
             self._animate()
             # self.__print_not_visited()
@@ -67,9 +69,6 @@ class Maze:
             sleep(self.ani_speed if self.ani_speed is not None else 0.05)
 
     def _break_walls_r(self, i, j, seed:int = 1, is_final_reached: bool = False) -> bool:
-        # if is_final_reached:
-        #     return True
-        start_cell = self._cells[0][0] if len(self._cells) != 0 and len(self._cells[0]) != 0 else None
         sequence_nbr = 7745698 * seed
 
         curr_cell: Cell = self._cells[i][j]
@@ -77,66 +76,44 @@ class Maze:
         if curr_cell.visited:
             return False
         curr_cell.visited = True
-        # if curr_cell == final_cell:
-            # return True
-        top_cell: Cell | None = self._cells[i][j-1] if j !=0 else None
-        bottom_cell: Cell | None = self._cells[i][j+1] if j < len(self._cells) - 1 else None
-        left_cell: Cell | None = self._cells[i-1][j] if i != 0 else None
-        right_cell: Cell | None = self._cells[i+1][j] if i < len(self._cells) - 1 else None
+        top_cell: Cell | None = self._cells[i-1][j] if i !=0 else None
+        bottom_cell: Cell | None = self._cells[i+1][j] if i < (self.__rows - 1) else None
+        left_cell: Cell | None = self._cells[i][j-1] if j != 0 else None
+        right_cell: Cell | None = self._cells[i][j+1] if j < (self.__cols - 1) else None
         wall_info = []
-        # neighbors = [top_cell, bottom_cell, left_cell, right_cell]
-        # if final_cell in neighbors:
-        #     print("final cell reached")
-        #     return True
-        # --TODO: open some random walls on the cell
-        if curr_cell.has_top_wall and j!=0:
+        if curr_cell.has_top_wall and i!=0:
             wall_info.append("top")
-        if curr_cell.has_left_wall and i!=0:
+        if curr_cell.has_left_wall and j!=0:
             wall_info.append("left")
-        if curr_cell.has_bottom_wall and j != (len(self._cells[0]) - 1):
+        if curr_cell.has_bottom_wall and i != (self.__rows - 1):
             wall_info.append("bottom")
-        if curr_cell.has_right_wall and i != (len(self._cells) - 1):
+        if curr_cell.has_right_wall and j != (self.__cols - 1):
             wall_info.append("right")
-        if curr_cell.has_top_wall and top_cell is not None and top_cell.has_bottom_wall:
+        if curr_cell.has_top_wall and top_cell is not None and not top_cell.has_bottom_wall:
             curr_cell.has_top_wall = False
-            curr_cell.draw()
-        if curr_cell.has_left_wall and left_cell is not None and left_cell.has_right_wall:
+        if curr_cell.has_left_wall and left_cell is not None and not left_cell.has_right_wall:
             curr_cell.has_left_wall = False
-            curr_cell.draw()
-        if curr_cell.has_bottom_wall and bottom_cell is not None and bottom_cell.has_top_wall:
+        if curr_cell.has_bottom_wall and bottom_cell is not None and not bottom_cell.has_top_wall:
             curr_cell.has_bottom_wall = False
-            curr_cell.draw()
-        if curr_cell.has_right_wall and right_cell is not None and right_cell.has_left_wall:
+        if curr_cell.has_right_wall and right_cell is not None and not right_cell.has_left_wall:
             curr_cell.has_right_wall = False
-            curr_cell.draw()
         if len(wall_info) != 0:
             rand_sel = randint(0, len(wall_info)-1)
-            print(f"cell {i},{j} will remove wall {wall_info[rand_sel]}")
-            self.__decide_wall(i, j, wall_info[rand_sel], False)
+            self.__decide_wall(i, j, wall_info[rand_sel])
         if top_cell is not None and not top_cell.visited:
-            self._break_walls_r(i, j-1)
-            # if is_final_reached:
-            #     return True
-        if left_cell is not None and not left_cell.visited:
             self._break_walls_r(i-1, j)
-            # if is_final_reached:
-            #     return True
+        if left_cell is not None and not left_cell.visited:
+            self._break_walls_r(i, j-1)
         if bottom_cell is not None and not bottom_cell.visited:
-            self._break_walls_r(i, j+1)
-            # if is_final_reached:
-            #     return True
-        if right_cell is not None and not right_cell.visited:
             self._break_walls_r(i+1, j)
-            # if is_final_reached:
-            #     return True
-        if self.__win is not None:
-            self.__win.redraw()
+        if right_cell is not None and not right_cell.visited:
+            self._break_walls_r(i, j+1)
+        curr_cell.draw()
         return False
 
     def _break_walls_rcopy(self, i, j, seed:int = 1, is_final_reached: bool = False) -> bool:
         start_cell = self._cells[0][0] if len(self._cells) != 0 and len(self._cells[0]) != 0 else None
         sequence_nbr = 7745698 * seed
-
         curr_cell: Cell = self._cells[i][j]
         final_cell = self._cells[len(self._cells) - 1][len(self._cells[0])-1]
         if curr_cell.visited:
@@ -151,9 +128,9 @@ class Maze:
             wall_info.append("top")
         if curr_cell.has_left_wall and i!=0:
             wall_info.append("left")
-        if curr_cell.has_bottom_wall and j != (len(self._cells[0]) - 1):
+        if curr_cell.has_bottom_wall and j != (self.__rows - 1):
             wall_info.append("bottom")
-        if curr_cell.has_right_wall and i != (len(self._cells) - 1):
+        if curr_cell.has_right_wall and i != (self.__cols - 1):
             wall_info.append("right")
         if curr_cell.has_top_wall and top_cell is not None and top_cell.has_bottom_wall:
             curr_cell.has_top_wall = False
@@ -190,26 +167,25 @@ class Maze:
                     if not self._cells[col][row].visited:
                         print(f"cell [{col}][{row}] is not visited")
 
-    def __decide_wall(self, i: int, j: int, wallname: str, has_wall: bool) -> None:
+    def __decide_wall(self, i: int, j: int, wallname: str) -> None:
         """
         helper function to set wall property based on its name
         """
-        cell = self._cells[i][j]
         match wallname:
             case "top":
-                cell.has_top_wall = has_wall
-                # print(f"top cell broken for cell[{i}][{j}]")
+                self._cells[i][j].has_top_wall = False
+                print(f"top cell broken for cell[{i}][{j}]")
             case "bottom":
-                cell.has_bottom_wall = has_wall
-                # print(f"bottom cell broken for cell[{i}][{j}]")
+                self._cells[i][j].has_bottom_wall = False
+                print(f"bottom cell broken for cell[{i}][{j}]")
             case "left":
-                cell.has_left_wall = has_wall
-                # print(f"left cell broken for cell[{i}][{j}]")
+                self._cells[i][j].has_left_wall = False
+                print(f"left cell broken for cell[{i}][{j}]")
             case "right":
-                cell.has_right_wall = has_wall
-                # print(f"right cell broken for cell[{i}][{j}]")
-        cell.draw()
-
+                self._cells[i][j].has_right_wall = False
+                print(f"right cell broken for cell[{i}][{j}]")
+            case _:
+                raise Exception("this case is not valid")
 
 
 def main():
