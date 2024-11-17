3a78e0d6e51218c294bd2c08918f4bd605452308
Vignesh Sekar <vigneshsekar1996@gmail.com>
2024-11-17 16:43:29 +0530

    WIP maze construction

--- main.py
+++ main.py
@@ -7,53 +7,9 @@ from maze import Maze
 
 def main():
     win = TWindow(800, 600)
-    # topleft_corner = (20,20)
-    # bottomright_corner = (1900,980)
-    # total_width = bottomright_corner[0] - topleft_corner[0]
-    # total_height = bottomright_corner[1] - topleft_corner[1]
-    # rows = 3
-    # columns = 3
-    # cell_width = total_width / rows
-    # cell_height = total_height / columns
-    # # total_cells = rows * columns
-    # cell_list: dict[str, Cell] = {}
-    # for col in range(columns):
-    #     for row in range(rows):
-    #         start_point = (int(topleft_corner[0] + (row * cell_width)), int(topleft_corner[1] + (col * cell_height)))
-    #         end_point = (int(start_point[0] + cell_width), int(start_point[1] + cell_height))
-    #         c = get_cell(win, [start_point, end_point, rand_walls(0.33)])
-    #         c.draw()
-    #         cell_list[str(row) + str(col)] = c
-
-    # for col in range(columns):
-    #     for row in range(rows):
-    #             cur_cell = cell_list[str(row) + str(col)]
-    #             topkey = str(row-1) + str(col) 
-    #             bottomkey = str(row+1) + str(col) 
-    #             rightkey = str(row) + str(col+1) 
-    #             leftkey = str(row) + str(col-1) 
-    #             # print(f"current cell index: {str(row) + str(col)}")
-    #             if topkey in cell_list:
-    #                 # print(f"topkey: {topkey}")
-    #                 cur_cell.draw_move(cell_list[topkey])
-    #             if bottomkey in cell_list:
-    #                 # print(f"bottomkey: {bottomkey}")
-    #                 cur_cell.draw_move(cell_list[bottomkey])
-    #             if rightkey in cell_list:
-    #                 # print(f"rightkey: {rightkey}")
-    #                 cur_cell.draw_move(cell_list[rightkey])
-    #             if leftkey in cell_list:
-    #                 # print(f"leftkey: {leftkey}")
-    #                 cur_cell.draw_move(cell_list[leftkey])
-    Maze(10,10, Point(10,10), 20, 20, win)
-
-    # outerbox = get_cell(win, [topleft_corner, bottomright_corner,[True,True,True,True]])
-    # outerbox.draw()
+    Maze(10,10, Point(10,10), 50, 50, win, 0.01)
 
     win.wait_for_close()
-
-def get_cell(win, cell) -> Cell:
-    return Cell(win=win, top_left=Point(cell[0][0],cell[0][1]), bottom_right=Point(cell[1][0],cell[1][1]), has_top_wall=cell[2][0], has_left_wall=cell[2][1], has_bottom_wall=cell[2][2], has_right_wall=cell[2][3])
 
 def rand_bool(true_prob = 0.5) -> bool:
     """
--- maze.py
+++ maze.py
@@ -8,7 +8,7 @@ from random import randint
 class Maze:
 
 
-    def __init__(self, rows: int, cols: int, start_pt: Point, cell_width: int, cell_height:int, win: TWindow | None = None) -> None:
+    def __init__(self, rows: int, cols: int, start_pt: Point, cell_width: int, cell_height:int, win: TWindow | None = None, ani_speed: float | None = None) -> None:
         """
         creates a maze box with provided row and column count. It takes a start_pt argument to specify the starting x,y co-ordinate of the maze.
         cell_width and cell_height specifies the width and height dimensions of the cell.
@@ -19,9 +19,14 @@ class Maze:
         self.__cell_width: int = cell_width
         self.__cell_height: int = cell_height
         self.__win: TWindow | None = win
+        self.ani_speed = ani_speed
         self._cells: list[list[Cell]] = []
         self._create_cells()
         self._break_entrance_and_exit()
+        if rows !=0 and cols!=0:
+            self._break_walls_r(0,0)
+            self._animate()
+            # self.__print_not_visited()
 
     def _create_cells(self) -> None:
         """
@@ -53,40 +58,119 @@ class Maze:
         self._animate()
         return cell
 
-    def _animate(self, ani_speed = 0.05) -> None:
+    def _animate(self) -> None:
         """
         animates the drawing of each cell
         """
         if self.__win is not None:
             self.__win.redraw()
-            sleep(ani_speed)
+            sleep(self.ani_speed if self.ani_speed is not None else 0.05)
 
-    def _break_walls_r(self, i, j) -> None:
+    def _break_walls_r(self, i, j, seed:int = 1, is_final_reached: bool = False) -> bool:
+        # if is_final_reached:
+        #     return True
+        start_cell = self._cells[0][0] if len(self._cells) != 0 and len(self._cells[0]) != 0 else None
+        sequence_nbr = 7745698 * seed
+
         curr_cell: Cell = self._cells[i][j]
+        final_cell = self._cells[len(self._cells) - 1][len(self._cells[0])-1]
         if curr_cell.visited:
-            return
+            return False
+        curr_cell.visited = True
+        # if curr_cell == final_cell:
+            # return True
         top_cell: Cell | None = self._cells[i][j-1] if j !=0 else None
         bottom_cell: Cell | None = self._cells[i][j+1] if j < len(self._cells) - 1 else None
         left_cell: Cell | None = self._cells[i-1][j] if i != 0 else None
         right_cell: Cell | None = self._cells[i+1][j] if i < len(self._cells) - 1 else None
         wall_info = []
-        neighbors = [top_cell, bottom_cell, left_cell, right_cell]
-        final_cell = self._cells[len(self._cells)-1][len(self._cells[0])-1]
-        if final_cell in neighbors:
-            print("final cell reached")
-            return
-        curr_cell.visited = True
+        # neighbors = [top_cell, bottom_cell, left_cell, right_cell]
+        # if final_cell in neighbors:
+        #     print("final cell reached")
+        #     return True
         # --TODO: open some random walls on the cell
-        if self._cells[i][j].has_top_wall:
+        if curr_cell.has_top_wall and j!=0:
             wall_info.append("top")
-        if self._cells[i][j].has_left_wall:
+        if curr_cell.has_left_wall and i!=0:
             wall_info.append("left")
-        if self._cells[i][j].has_bottom_wall:
+        if curr_cell.has_bottom_wall and j != (len(self._cells[0]) - 1):
             wall_info.append("bottom")
-        if self._cells[i][j].has_right_wall:
+        if curr_cell.has_right_wall and i != (len(self._cells) - 1):
             wall_info.append("right")
-        rand_sel = randint(0, len(wall_info))
-        self.__decide_wall(self._cells[i][j], wall_info[rand_sel], False)
+        if curr_cell.has_top_wall and top_cell is not None and top_cell.has_bottom_wall:
+            curr_cell.has_top_wall = False
+            curr_cell.draw()
+        if curr_cell.has_left_wall and left_cell is not None and left_cell.has_right_wall:
+            curr_cell.has_left_wall = False
+            curr_cell.draw()
+        if curr_cell.has_bottom_wall and bottom_cell is not None and bottom_cell.has_top_wall:
+            curr_cell.has_bottom_wall = False
+            curr_cell.draw()
+        if curr_cell.has_right_wall and right_cell is not None and right_cell.has_left_wall:
+            curr_cell.has_right_wall = False
+            curr_cell.draw()
+        if len(wall_info) != 0:
+            rand_sel = randint(0, len(wall_info)-1)
+            print(f"cell {i},{j} will remove wall {wall_info[rand_sel]}")
+            self.__decide_wall(i, j, wall_info[rand_sel], False)
+        if top_cell is not None and not top_cell.visited:
+            self._break_walls_r(i, j-1)
+            # if is_final_reached:
+            #     return True
+        if left_cell is not None and not left_cell.visited:
+            self._break_walls_r(i-1, j)
+            # if is_final_reached:
+            #     return True
+        if bottom_cell is not None and not bottom_cell.visited:
+            self._break_walls_r(i, j+1)
+            # if is_final_reached:
+            #     return True
+        if right_cell is not None and not right_cell.visited:
+            self._break_walls_r(i+1, j)
+            # if is_final_reached:
+            #     return True
+        if self.__win is not None:
+            self.__win.redraw()
+        return False
+
+    def _break_walls_rcopy(self, i, j, seed:int = 1, is_final_reached: bool = False) -> bool:
+        start_cell = self._cells[0][0] if len(self._cells) != 0 and len(self._cells[0]) != 0 else None
+        sequence_nbr = 7745698 * seed
+
+        curr_cell: Cell = self._cells[i][j]
+        final_cell = self._cells[len(self._cells) - 1][len(self._cells[0])-1]
+        if curr_cell.visited:
+            return False
+        curr_cell.visited = True
+        top_cell: Cell | None = self._cells[i][j-1] if j !=0 else None
+        bottom_cell: Cell | None = self._cells[i][j+1] if j < len(self._cells) - 1 else None
+        left_cell: Cell | None = self._cells[i-1][j] if i != 0 else None
+        right_cell: Cell | None = self._cells[i+1][j] if i < len(self._cells) - 1 else None
+        wall_info = []
+        if curr_cell.has_top_wall and j!=0:
+            wall_info.append("top")
+        if curr_cell.has_left_wall and i!=0:
+            wall_info.append("left")
+        if curr_cell.has_bottom_wall and j != (len(self._cells[0]) - 1):
+            wall_info.append("bottom")
+        if curr_cell.has_right_wall and i != (len(self._cells) - 1):
+            wall_info.append("right")
+        if curr_cell.has_top_wall and top_cell is not None and top_cell.has_bottom_wall:
+            curr_cell.has_top_wall = False
+            curr_cell.draw()
+        if curr_cell.has_left_wall and left_cell is not None and left_cell.has_right_wall:
+            curr_cell.has_left_wall = False
+            curr_cell.draw()
+        if curr_cell.has_bottom_wall and bottom_cell is not None and bottom_cell.has_top_wall:
+            curr_cell.has_bottom_wall = False
+            curr_cell.draw()
+        if curr_cell.has_right_wall and right_cell is not None and right_cell.has_left_wall:
+            curr_cell.has_right_wall = False
+            curr_cell.draw()
+        if len(wall_info) != 0:
+            rand_sel = randint(0, len(wall_info)-1)
+            print(f"cell {i},{j} will remove wall {wall_info[rand_sel]}")
+            self.__decide_wall(i, j, wall_info[rand_sel], False)
         if top_cell is not None and not top_cell.visited:
             self._break_walls_r(i, j-1)
         if left_cell is not None and not left_cell.visited:
@@ -95,22 +179,36 @@ class Maze:
             self._break_walls_r(i, j+1)
         if right_cell is not None and not right_cell.visited:
             self._break_walls_r(i+1, j)
+        if self.__win is not None:
+            self.__win.redraw()
+        return False
 
+    def __print_not_visited(self):
+        if len(self._cells) != 0 and len(self._cells[0]) !=0:
+            for col in range(len(self._cells)):
+                for row in range(len(self._cells[0])):
+                    if not self._cells[col][row].visited:
+                        print(f"cell [{col}][{row}] is not visited")
 
-
-    def __decide_wall(self, cell: Cell, wallname: str, has_wall: bool) -> None:
+    def __decide_wall(self, i: int, j: int, wallname: str, has_wall: bool) -> None:
         """
         helper function to set wall property based on its name
         """
+        cell = self._cells[i][j]
         match wallname:
             case "top":
                 cell.has_top_wall = has_wall
+                # print(f"top cell broken for cell[{i}][{j}]")
             case "bottom":
                 cell.has_bottom_wall = has_wall
+                # print(f"bottom cell broken for cell[{i}][{j}]")
             case "left":
                 cell.has_left_wall = has_wall
+                # print(f"left cell broken for cell[{i}][{j}]")
             case "right":
                 cell.has_right_wall = has_wall
+                # print(f"right cell broken for cell[{i}][{j}]")
+        cell.draw()
 
 
 
